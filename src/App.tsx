41f6086a8c737df50790908ed8bfb3ae55c9cd8a
Marcelo Melo <marcelosdm@gmail.com>
2020-07-01 16:41:37 -0300

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import 'react-native-gesture-handler';
 
-const App: React.FC = () => {
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ flex: 1, backgroundColor: "#312e38" }} />;
-    </>
-  );
-};
+import React from "react";
+import { View, StatusBar } from 'react-native'
+
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: "#312e38" }} />;
+  </>
+);
+
 
 export default App;
